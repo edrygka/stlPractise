f1024b5eb0c1c852d8a9781822ba09386dcf9d84
edrygka <edrygha@gmail.com>
2018-06-16 11:46:29 +0300

    ended work with OOP_lab2(STL)

--- lab_STL/main.cpp
+++ lab_STL/main.cpp
@@ -25,10 +25,26 @@ void showList(list<char> l, string s){
     cout << "'" << endl;
 }
 
-
 void reverseOfList(list<char> l){
     l.reverse();
     showList(l, "reverse");
+}
+
+char getElementByIndex(int index, list<char> l){
+    try{
+        if(index > l.size()) throw 404;
+        list<char>::iterator p = l.begin();
+        int n = 0;
+        while (n != index) {
+            p++;
+            n++;
+        }
+        return *p;
+    }
+    catch (int n){
+        cout << n << endl;
+        return ' ';
+    }
 }
 
 int main(int argc, const char * argv[]) {
@@ -36,16 +52,16 @@ int main(int argc, const char * argv[]) {
     // Define variable
     list<char> repiatList, nrList;
     vector<char>::iterator it1;
-    list<char>::iterator it2;
-    int sizeOfString, index = 0, summary = 0;
+    list<char>::iterator it2, it3;
+    int sizeOfString, index = 0;
     
     // Get from stdin size of string will entered user
-    cout << "Input size of string " << endl;
+    cout << "Input size of string = ";
     cin >> sizeOfString;
     cin.get();
     
     // Get string from stdin
-    cout << "Input string " << endl;
+    cout << "Input string = ";
     char name[sizeOfString];
     cin.getline (name, sizeOfString+1);
     vector<char> newArr(sizeof(name));
@@ -63,17 +79,23 @@ int main(int argc, const char * argv[]) {
     cout << "'" << endl;
     it1 = newArr.begin();
     for(it1 = newArr.begin(); it1 < newArr.end(); it1++){
+        it2 = find(repiatList.begin(), repiatList.end(), *(it1));
+        it3 = find(nrList.begin(), nrList.end(), *(it1));
         nrList.push_back(*(it1));
         if(index == 0){
             index++;
             continue;
         }
+        
+        if(*it3 == *it1 || *it2 == *it1){
+            nrList.remove(*(it1));
+            nrList.remove(*(it1));
+        }
+        
         if(*(it1-1) == *it1){
-            it2 = find(repiatList.begin(), repiatList.end(), *(it1));
             if(*it2 == *(it1-1)) {
-                nrList.remove(*(it1-1));
-            }
-            else {
+                nrList.remove(*(it1));
+            } else {
                 repiatList.push_back(*(it1));
             }
         }
@@ -82,6 +104,7 @@ int main(int argc, const char * argv[]) {
     showList(repiatList, "repeat");
     cout << getSize(nrList) << endl;
     reverseOfList(nrList);
+    cout << getElementByIndex(3, repiatList) << endl;
     cout << "\nHello, World!" << endl;
     return 0;
 }
